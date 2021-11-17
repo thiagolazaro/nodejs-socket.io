abc72c019ad7952b692c8e9a1a95754ca9862168
Thiago Lázaro <thiagolazaro2009@gmail.com>
2021-11-16 22:26:48 -0300

    Enviando projeto para Github

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto de estudo NodeJS e Socket.IO
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { serverHttp } from "./http";
+import "./websocket";
+
+
+serverHttp.listen(3000, () => console.log("Server is running on Port 3000 🏆🏆🏆🏆"))
